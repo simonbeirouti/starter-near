faf65aa04c3a0fdf4900f4b9cd0c8f333732ab89
simonbeirouti <hello@simonbeirouti.com>
2024-10-29 20:50:59 +0700

    setup auction page

--- src/app/app/auction/page.tsx
+++ src/app/app/auction/page.tsx
@@ -1,14 +1,43 @@
-import type { Metadata } from "next";
+import {Overview} from "@/components/overview";
+import {RecentSales} from "@/components/recent-sales";
+import type {Metadata} from "next";
+import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card";
 
 export const metadata: Metadata = {
-  title: "Bitte Starter with Next.js",
-  description: "Simple Login with Next.js 14",
+	title: "Bitte Starter with Next.js",
+	description: "Simple Login with Next.js 14",
 };
 
 export default function AccountPage() {
-  return (
-    <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      Account
-    </main>
-  );
+	return (
+		<main className="flex flex-col">
+			<Data />
+		</main>
+	);
+}
+
+function Data() {
+	return (
+		<div className="grid gap-4 md:grid-cols-4 lg:grid-cols-8">
+			<Card className="col-span-4">
+				<CardHeader>
+					<CardTitle>Overview</CardTitle>
+				</CardHeader>
+				<CardContent className="pl-2">
+					<Overview />
+				</CardContent>
+			</Card>
+			<Card className="col-span-4">
+				<CardHeader>
+					<CardTitle>Recent Sales</CardTitle>
+					<CardDescription>
+						You made 265 sales this month.
+					</CardDescription>
+				</CardHeader>
+				<CardContent>
+					<RecentSales />
+				</CardContent>
+			</Card>
+		</div>
+	);
 }
