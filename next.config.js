87b6dcc662ff53a7a6afb27c7c83c592227df3ea
simonbeirouti <hello@simonbeirouti.com>
2024-10-30 19:32:41 +0700

    attempt fix

--- next.config.js
+++ next.config.js
@@ -1,24 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async rewrites() {
-    const isLocalhost = process.env.NODE_ENV === 'development';
-    const appDomain = process.env.NEXT_PUBLIC_APP_DOMAIN || 'app.localhost:3000';
+const nextConfig = {};
 
-    return {
-      beforeFiles: [
-        {
-          source: '/:path*',
-          has: [
-            {
-              type: 'host',
-              value: isLocalhost ? 'app.localhost:3000' : appDomain,
-            },
-          ],
-          destination: '/app/:path*',
-        },
-      ],
-    }
-  },
-}
-
-module.exports = nextConfig
+module.exports = nextConfig;
